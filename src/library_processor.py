febf4cc48c0b375cfb991f4dc989746bd1973dea
Don <4340360+ilDon@users.noreply.github.com>
2023-04-23 16:47:50 +0200

    feat: added logic for excluded files

--- src/library_processor.py
+++ src/library_processor.py
@@ -1,11 +1,10 @@
 import os
 import glob
 import PyPDF2
-import docx2txt
 import numpy as np
 from typing import List
-from sklearn.metrics.pairwise import cosine_similarity
 import json
+import resource
 
 from src.database import DatabaseHelper
 from src.embedder import getTextEmbedding
@@ -14,38 +13,52 @@ from src.const import MAX_SECTION_CHARS
 class LibraryProcessor:
 
     def __init__(self, search_folder="test_files"):
+        soft_limit, hard_limit = resource.getrlimit(resource.RLIMIT_NOFILE)
+        resource.setrlimit(resource.RLIMIT_NOFILE, (hard_limit, hard_limit))
+
         self.search_folder = search_folder
         db_folder = os.path.dirname(os.path.abspath(search_folder))
         DatabaseHelper.init(db_folder=db_folder)
         DatabaseHelper.write("""CREATE TABLE IF NOT EXISTS embeddings (
                       id TEXT, file_name TEXT, section_number INTEGER, 
                       embedding TEXT)""")
-        self.processed_folder_ids = self.get_processed_folder_ids()
+        DatabaseHelper.write("""CREATE TABLE IF NOT EXISTS excluded (
+                      id TEXT, reason TEXT)""")
+        self.processed_folder_ids = self.get_processed_folder_ids("embeddings")
+        self.excluded_folder_ids = self.get_processed_folder_ids("excluded")
 
-    def get_processed_folder_ids(self):
+    def get_processed_folder_ids(self, table_name: str):
         folder_ids = set()
-        rows = DatabaseHelper.read("SELECT DISTINCT id FROM embeddings")
+        rows = DatabaseHelper.read(f"SELECT DISTINCT id FROM {table_name}")
         for row in rows:
             folder_ids.add(row[0])
         return folder_ids
 
-    def extract_text_from_file(self, file: str) -> str:
+    def extract_text_from_file(self, file: str, folder_id: str) -> str:
         file_text = ""
-        if file.endswith(".pdf"):
-            with open(file, "rb") as pdf_file:
-                with open(os.devnull, 'w') as devnull:
-                    original_stderr = os.dup(2)
-                    os.dup2(devnull.fileno(), 2)
-                    try:
-                        reader = PyPDF2.PdfReader(pdf_file)
+        with open(file, "rb") as pdf_file:
+            with open(os.devnull, 'w') as devnull:
+                original_stderr = os.dup(2)
+                os.dup2(devnull.fileno(), 2)
+                try:
+                    print(f"\n\nProcessing file: {pdf_file.name}\n")
+                    print(f"Folder: {folder_id}\n")
+                    reader = PyPDF2.PdfReader(pdf_file)
+                    if not reader.is_encrypted:  # Check if the PDF is not encrypted
                         len_pages = len(reader.pages)
                         for page in range(len_pages):
                             file_text += reader.pages[page].extract_text()
-                    finally:
-                        os.dup2(original_stderr, 2)
-        elif file.endswith(".docx"):
-            file_text = docx2txt.process(file)
-        
+                    else:
+                        # add to excluded in DB
+                        DatabaseHelper.write("INSERT INTO excluded VALUES (?, ?)",
+                                             (folder_id, "encrypted"))
+                        print(f"\n\nSkipping encrypted file: {pdf_file.name}\n")
+                finally:
+                    reader = None
+                    os.dup2(original_stderr, 2)
+                    devnull.close()
+                    pdf_file.close()
+      
         return file_text
 
     def split_and_truncate(self, text: str) -> List[str]:
@@ -60,18 +73,32 @@ class LibraryProcessor:
 
     def process_files(self):
         path_to_scan = f"{self.search_folder}/*"
-        total_len = len(glob.glob(path_to_scan))
-        i = 0
+        
+        # Generate a list of all PDF files
+        pdf_files = []
         for folder in glob.glob(path_to_scan):
-            print(f'\r- Processing file: {i + 1}/{total_len}', end='')
-            i += 1
             folder_id = os.path.basename(folder)
-            if folder_id in self.processed_folder_ids:
+            if folder_id in self.processed_folder_ids or folder_id in self.excluded_folder_ids:
                 continue
             for file in glob.glob(f"{folder}/*"):
-                file_name = os.path.basename(file)
-                file_text = self.extract_text_from_file(file)
-                sections = self.split_and_truncate(file_text)
-                embeddings = getTextEmbedding(sections)
-                self.save_embeddings(folder_id, file_name, sections, embeddings)
+                if file.endswith(".pdf"):
+                    pdf_files.append((folder, file))
+
+        # Process the list of PDF files
+        total_len = len(pdf_files)
+        for i, (folder, file) in enumerate(pdf_files):
+            print(f'\r- Processing file: {i + 1}/{total_len}', end='')
+
+            folder_id = os.path.basename(folder)
+            file_name = os.path.basename(file)
+            file_text = self.extract_text_from_file(file, folder_id)
+            sections = self.split_and_truncate(file_text)
+
+            # Check sections length is > 0 and all sections are not empty
+            if len(sections) == 0 or all([len(section) == 0 for section in sections]):
+                continue
+
+            embeddings = getTextEmbedding(sections)
+            self.save_embeddings(folder_id, file_name, sections, embeddings)
+
         
