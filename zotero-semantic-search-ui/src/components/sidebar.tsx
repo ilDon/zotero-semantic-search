89fe134a3154c8db97989f71b053f2fff0a54b0f
Don <4340360+ilDon@users.noreply.github.com>
2023-04-23 17:03:37 +0200

    chore: removed redundant prop

--- zotero-semantic-search-ui/src/components/sidebar.tsx
+++ zotero-semantic-search-ui/src/components/sidebar.tsx
@@ -20,9 +20,9 @@ export const Sidebar: React.FC = () => {
         Zotero Semantic Search
       </div>
       <nav className="flex flex-1 flex-col">
-        <ul role="list" className="flex flex-1 flex-col gap-y-7">
+        <ul className="flex flex-1 flex-col gap-y-7">
           <li>
-            <ul role="list" className="-mx-2 space-y-1">
+            <ul className="-mx-2 space-y-1">
               {navigation.map((item) => (
                 <li key={item.name}>
                   <a
