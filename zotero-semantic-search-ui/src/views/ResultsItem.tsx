13aff4c314525af5dd1062203891f9eafbc31af1
Don <4340360+ilDon@users.noreply.github.com>
2023-04-28 10:16:31 +0200

    feat: auto close accordion

--- zotero-semantic-search-ui/src/views/ResultsItem.tsx
+++ zotero-semantic-search-ui/src/views/ResultsItem.tsx
@@ -57,48 +57,53 @@ export const ResultsItem: React.FC<IResultsItemProps> = (props: IResultsItemProp
         leaveTo="transform scale-95 opacity-0"
       >
         <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-          <div className="overflow-hidden bg-white">
-            <div className="">
-              <div className="flex space-x-3">
-                <div className="min-w-0 flex-1">
-                  <p className="text-sm font-semibold text-gray-900">
-                    Score: {props.result.similarity}
-                    <span className="ml-2 text-sm text-gray-500">
-                      Section: {props.result.section_number} - {props.folderId}
-                    </span>
-                  </p>
+            {({ close }) => (
+              <div className="overflow-hidden bg-white">
+                <div className="">
+                  <div className="flex space-x-3">
+                    <div className="min-w-0 flex-1">
+                      <p className="text-sm font-semibold text-gray-900">
+                        Score: {props.result.similarity}
+                        <span className="ml-2 text-sm text-gray-500">
+                          Section: {props.result.section_number} - {props.folderId}
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                  {!!sectionsText?.[props.result.section_number] && (
+                    <div className="pb-5 text-gray-500">
+                      <p>{sectionsText[props.result.section_number]}</p>
+                    </div>
+                  )}
+                  {!sectionsText?.[props.result.section_number] && (
+                    <p className="my-4">Loading...</p>
+                  )}
+                  <div className="flex justify-between">
+                    <button
+                      type="button"
+                      className="rounded-md bg-indigo-600 disabled:bg-indigo-500 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      onClick={handleOpenFile}
+                    >
+                      Open PDF
+                    </button>
+                    <select
+                      name="status"
+                      id="status"
+                      className="mt-2 rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      value={resultStatus}
+                      onChange={(e) => {
+                        handleSetStatus(parseInt(e.target.value));
+                        close();
+                      }}
+                    >
+                      <option value={Status.todo}>Todo</option>
+                      <option value={Status.analyzed}>Analyzed</option>
+                      <option value={Status.irrelevant}>Irrelevant</option>
+                    </select>
+                  </div>
                 </div>
               </div>
-              {!!sectionsText?.[props.result.section_number] && (
-                <div className="pb-5 text-gray-500">
-                  <p>{sectionsText[props.result.section_number]}</p>
-                </div>
-              )}
-              {!sectionsText?.[props.result.section_number] && (
-                <p className="my-4">Loading...</p>
-              )}
-              <div className="flex justify-between">
-                <button
-                  type="button"
-                  className="rounded-md bg-indigo-600 disabled:bg-indigo-500 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  onClick={handleOpenFile}
-                >
-                  Open PDF
-                </button>
-                <select
-                  name="status"
-                  id="status"
-                  className="mt-2 rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  value={resultStatus}
-                  onChange={(e) => handleSetStatus(parseInt(e.target.value))}
-                >
-                  <option value={Status.todo}>Todo</option>
-                  <option value={Status.analyzed}>Analyzed</option>
-                  <option value={Status.irrelevant}>Irrelevant</option>
-                </select>
-              </div>
-            </div>
-          </div>
+          )}
         </Disclosure.Panel>
       </Transition>
     </Disclosure>
--- zotero-semantic-search-ui/src/views/excluded-add.tsx
+++ zotero-semantic-search-ui/src/views/excluded-add.tsx
@@ -41,60 +41,70 @@ export const ExcludedAdd: React.FC<IExcludedAddProps> = (props) => {
           leaveTo="transform scale-95 opacity-0"
         >
           <Disclosure.Panel className="px-4 pt-4 pb-4 text-sm text-gray-500 border border-gray-900/10 mt-2 rounded-lg">
-            <div className="">
-              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:flex md:items-center">
-                <div className="md:grow">
-                  <label htmlFor="folderId" className="block text-sm font-medium leading-6 text-gray-900">
-                    Folder ID
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      type="text"
-                      name="folderId"
-                      id="folderId"
-                      value={folderId}
-                      autoComplete="off"
-                      onChange={(e) => setFolderId(e.target.value)}
-                      className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      placeholder="Enter folder ID"
-                    />
+            {({ close }) => (
+              <>
+                <div className="">
+                  <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:flex md:items-center">
+                    <div className="md:grow">
+                      <label htmlFor="folderId" className="block text-sm font-medium leading-6 text-gray-900">
+                        Folder ID
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="text"
+                          name="folderId"
+                          id="folderId"
+                          value={folderId}
+                          autoComplete="off"
+                          onChange={(e) => setFolderId(e.target.value)}
+                          className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          placeholder="Enter folder ID"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="md:w-40">
+                      <label htmlFor="mode" className="block text-sm font-medium leading-6 text-gray-900">
+                        Reason
+                      </label>
+                      <div className="mt-2">
+                        <select
+                          id="mode"
+                          name="mode"
+                          value={reason}
+                          onChange={(e) => setReason(e.target.value as IExcludedFolder['reason'])}
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                        >
+                          <option selected={reason === 'encrypted'} value="encrypted">encrypted</option>
+                          <option selected={reason === 'no_text'} value="no_text">no_text</option>
+                          <option selected={reason === 'manual'} value="manual">manual</option>
+                        </select>
+                      </div>
+                    </div>
                   </div>
                 </div>
 
-                <div className="md:w-40">
-                  <label htmlFor="mode" className="block text-sm font-medium leading-6 text-gray-900">
-                    Reason
-                  </label>
-                  <div className="mt-2">
-                    <select
-                      id="mode"
-                      name="mode"
-                      value={reason}
-                      onChange={(e) => setReason(e.target.value as IExcludedFolder['reason'])}
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                    >
-                      <option selected={reason === 'encrypted'} value="encrypted">encrypted</option>
-                      <option selected={reason === 'no_text'} value="no_text">no_text</option>
-                      <option selected={reason === 'manual'} value="manual">manual</option>
-                    </select>
-                  </div>
+                <div className="mt-6 flex items-center justify-end gap-x-6">
+                  <button type="button" className="text-sm font-semibold leading-6 text-gray-900" onClick={() => {
+                    onCancel();
+                    close();
+                  }}>
+                    Cancel
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      handleFormSubmit();
+                      close();
+                    }}
+                    disabled={isRemoving}
+                    className="rounded-md bg-indigo-600 disabled:bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Save
+                  </button>
                 </div>
-              </div>
-            </div>
-
-            <div className="mt-6 flex items-center justify-end gap-x-6">
-              <button type="button" className="text-sm font-semibold leading-6 text-gray-900" onClick={onCancel}>
-                Cancel
-              </button>
-              <button
-                type="button"
-                onClick={handleFormSubmit}
-                disabled={isRemoving}
-                className="rounded-md bg-indigo-600 disabled:bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Save
-              </button>
-            </div>
+              </>
+            )}
           </Disclosure.Panel>
         </Transition>
       </Disclosure>
