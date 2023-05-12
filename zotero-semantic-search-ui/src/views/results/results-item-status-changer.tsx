4b0b8479304564c056dd1c2575fd4102f9601660
Don <4340360+ilDon@users.noreply.github.com>
2023-05-12 16:21:06 +0200

    improved spacing

--- zotero-semantic-search-ui/src/views/results/results-item-status-changer.tsx
+++ zotero-semantic-search-ui/src/views/results/results-item-status-changer.tsx
@@ -23,7 +23,7 @@ export const ResultsItemStatusChanger: React.FC<IResultsItemProps> = (props: IRe
     <select
       name="status"
       id="status"
-      className="rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      className="ml-2 rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
       value={props.resultStatus}
       onChange={(e) => {
         handleSetStatus(parseInt(e.target.value));
