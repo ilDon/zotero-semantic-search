7c5d411a2d56992cd260fc20c0d8258bca9acf17
Don <4340360+ilDon@users.noreply.github.com>
2023-05-12 16:20:43 +0200

    autoclose

--- zotero-semantic-search-ui/src/views/results/results-item.tsx
+++ zotero-semantic-search-ui/src/views/results/results-item.tsx
@@ -38,81 +38,88 @@ export const ResultsItem: React.FC<IResultsItemProps> = (props: IResultsItemProp
   return (
   <div className="mb-2">
     <Disclosure>
-      <div className="flex w-full items-center justify-center">
-        <Disclosure.Button className={`flex grow justify-between rounded-lg px-4 py-2 text-left text-sm font-medium focus:outline-none focus-visible:ring focus-visible:ring-opacity-75 mr-2 ${disclosureColors}`}>
-          <FileNameWithId
-            fileName={props.result?.file_name}
-            id={props.folderId}
-          />
-        </Disclosure.Button>
-        <ResultsItemStatusChanger
-          resultStatus={resultStatus}
-          result={props.result}
-          index={props.index}
-          historyElementId={props.historyElementId}
-          setResultStatus={setResultStatus}
-        />
-      </div>
+        {({ close }) => (
+          <>
+            <div className="flex w-full items-center justify-center">
+            <Disclosure.Button className={`flex grow justify-between rounded-lg px-4 py-2 text-left text-sm font-medium focus:outline-none focus-visible:ring focus-visible:ring-opacity-75 ${disclosureColors}`}>
+              <FileNameWithId
+                fileName={props.result?.file_name}
+                id={props.folderId}
+              />
+            </Disclosure.Button>
+            {!!sectionsText?.[props.result.section_number] && (
+              <ResultsItemGeneratePrompt text={sectionsText?.[props.result.section_number]} />
+            )}
+            <ResultsItemStatusChanger
+              resultStatus={resultStatus}
+              result={props.result}
+              index={props.index}
+              historyElementId={props.historyElementId}
+              setResultStatus={setResultStatus}
+              close={close}
+            />
+          </div>
+        
 
-      <Transition
-        enter="transition duration-100 ease-out"
-        enterFrom="transform scale-95 opacity-0"
-        enterTo="transform scale-100 opacity-100"
-        leave="transition duration-75 ease-out"
-        leaveFrom="transform scale-100 opacity-100"
-        leaveTo="transform scale-95 opacity-0"
-      >
-        <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-            {({ close }) => (
-              <div className="overflow-hidden bg-white">
-                <div className="">
-                  <div className="flex space-x-3">
-                    <div className="min-w-0 flex-1">
-                      <FileNameWithId
-                        fileName={props.result?.file_name}
-                        id={props.folderId}
-                      />
-                      <p className="text-sm font-semibold text-gray-900">
-                        Score: {props.result.similarity}
-                        <span className="ml-2 text-sm text-gray-500">
-                          Section: {props.result.section_number} - {props.folderId}
-                        </span>
-                      </p>
+          <Transition
+            enter="transition duration-100 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+          >
+            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                <div className="overflow-hidden bg-white">
+                  <div className="">
+                    <div className="flex space-x-3">
+                      <div className="min-w-0 flex-1">
+                        <FileNameWithId
+                          fileName={props.result?.file_name}
+                          id={props.folderId}
+                        />
+                        <p className="text-sm font-semibold text-gray-900">
+                          Score: {props.result.similarity}
+                          <span className="ml-2 text-sm text-gray-500">
+                            Section: {props.result.section_number} - {props.folderId}
+                          </span>
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                  {!!sectionsText?.[props.result.section_number] && (
-                    <ResultsItemText text={sectionsText[props.result.section_number]} />
-                    )}
-                  {!sectionsText?.[props.result.section_number] && (
-                    <p className="my-4 text-gray-600">Loading...</p>
-                    )}
-                  <div className="flex justify-between">
-                    <div className="flex items-center justify-start">
-                      <button
-                        type="button"
-                        className="rounded-md bg-indigo-600 disabled:bg-indigo-500 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        onClick={handleOpenFile}
-                        >
-                        Open PDF
-                      </button>
-                      {!!sectionsText?.[props.result.section_number] && (
-                        <ResultsItemGeneratePrompt text={sectionsText?.[props.result.section_number]} />
+                    {!!sectionsText?.[props.result.section_number] && (
+                      <ResultsItemText text={sectionsText[props.result.section_number]} />
                       )}
+                    {!sectionsText?.[props.result.section_number] && (
+                      <p className="my-4 text-gray-600">Loading...</p>
+                      )}
+                    <div className="flex justify-between">
+                      <div className="flex items-center justify-start">
+                        <button
+                          type="button"
+                          className="rounded-md bg-indigo-600 disabled:bg-indigo-500 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                          onClick={handleOpenFile}
+                          >
+                          Open PDF
+                        </button>
+                        {!!sectionsText?.[props.result.section_number] && (
+                          <ResultsItemGeneratePrompt text={sectionsText?.[props.result.section_number]} />
+                        )}
+                      </div>
+                      <ResultsItemStatusChanger
+                        resultStatus={resultStatus}
+                        result={props.result}
+                        index={props.index}
+                        historyElementId={props.historyElementId}
+                        setResultStatus={setResultStatus}
+                        close={close}
+                      />
                     </div>
-                    <ResultsItemStatusChanger
-                      resultStatus={resultStatus}
-                      result={props.result}
-                      index={props.index}
-                      historyElementId={props.historyElementId}
-                      setResultStatus={setResultStatus}
-                      close={close}
-                    />
                   </div>
                 </div>
-              </div>
-          )}
-        </Disclosure.Panel>
-      </Transition>
+            </Disclosure.Panel>
+          </Transition>
+        </>
+      )}
     </Disclosure>
   </div>
 )
