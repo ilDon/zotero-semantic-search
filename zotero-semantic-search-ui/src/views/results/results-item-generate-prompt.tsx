b50cbda1ffb4ff9a7fd24fa4c9cd47b33d0a0857
Don <4340360+ilDon@users.noreply.github.com>
2023-05-15 11:18:49 +0200

    improved prompt

--- zotero-semantic-search-ui/src/views/results/results-item-generate-prompt.tsx
+++ zotero-semantic-search-ui/src/views/results/results-item-generate-prompt.tsx
@@ -5,10 +5,7 @@ interface IResultsItemProps {
   text: string
 }
 
-const PROMPT = `I am a university professor of law. I am writing a paper. The following is a section of my paper (SECTION), after that there is an excerpt from a paper that might be relevant in relation to what I wrote (EXCERPT). 
-Please assess if the excerpt contains any concept that directly support the contents of SECTION. The concepts must be directly relevant.
-If so, please reply by stating which part of the excerpt I should quote and where I should add the footnote in my SECTION. Skip prose.
-If not, please reply: "no relevant text to quote".
+const PROMPT = `I am a university professor of law. I am writing a paper. The following is a section of my paper (SECTION), after that there is an excerpt from a paper that might be relevant in relation to what I wrote (EXCERPT). Please explain in one short sentence how the EXCERPT could support the contents of SECTION.
 
 SECTION:
 [QUERY]
